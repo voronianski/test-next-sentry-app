cd663e695b0257cbd6da8f9f290e85b79e7b6cd1
Dmitri Voronianskyi <dmitri.voronianskyi@sumup.com>
2021-12-08 16:52:26 +0100

    ignore map files

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Index = () => (
   <div style={{ maxWidth: 700, margin: '0 auto' }}>
-    <h2>Sentry Simple Example 🚨</h2>
+    <h2>Sentry Simple Example (updated) 🚨</h2>
     <p>
       This example demonstrates how to record unhandled exceptions in your code
       with Sentry. There are several test pages below that result in various
